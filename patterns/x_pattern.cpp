92eb3017aa9f1aa3d70617db23b2a5b43b97c151
shuvalt <107667260+shuvalt@users.noreply.github.com>
2023-10-10 22:30:17 +0530

    Update x_pattern.cpp

--- patterns/x_pattern.cpp
+++ patterns/x_pattern.cpp
@@ -1,1 +1,22 @@
-
+// x pattern for odd length string
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    
+    string name;
+    cin >> name;
+    int n = name.length();
+    
+    for (int i = 0; i <= n; i++) { 
+        for (int j = 0; j <= n; j++) { 
+            if (j == i) 
+                cout << name[i]; 
+            else if(j == n-i-1)
+                cout << name[j];
+            else
+                cout << " "; 
+        }
+        cout << endl;
+}
+}
