c821a1a97a0f71e675572798d82f6790294793b6
shuvalt <107667260+shuvalt@users.noreply.github.com>
2024-01-11 17:02:30 +0530

    Create binarySearch.py
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ array/binarySearch.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        while(left<=right):
+            mid = (right + left) // 2
+            if(nums[mid]<target):
+                left = mid+1
+            elif(nums[mid]>target):
+                right = mid-1
+            else:
+                return mid
+        return left
+        
+        
