8a80b0fdf96cdf388f93acf0421b8dbaef3cd084
shuvalt <107667260+shuvalt@users.noreply.github.com>
2024-01-18 23:59:26 +0530

    Update sorting_token.py

--- string/sorting_token.py
+++ string/sorting_token.py
@@ -19,3 +19,25 @@ print(n)
 print(string)
 print(reverse)
 print(compare)
+
+#sorting portion
+def numsort(s1):
+    key1 = s1[1]
+    return int(key1)
+
+def lexisort(e):
+    key2 = e[1]
+    return key2
+    
+    
+
+string = [["89","13"],["98","022"],["102","12"]]
+rev = True
+ty = 'l'
+
+if(ty == 'n'):
+    string.sort(reverse=rev,key=numsort)
+else:
+    string.sort(reverse=rev,key=lexisort)
+
+print(string)
