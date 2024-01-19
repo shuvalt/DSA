8705b0e1dd25b598803e51e0c2c64223336bf08a
shuvalt <107667260+shuvalt@users.noreply.github.com>
2024-01-19 23:48:25 +0530

    Create fizzBuzz.py

---
+++ slidingWindow/fizzBuzz.py
@@ -0,0 +1,12 @@
+def fizzBuzz(self, n):
+        result = []
+        for i in range(n):
+            if((i+1)%3 == 0 and (i+1)%5 == 0):
+                result.append("FizzBuzz")
+            elif((i+1)%3 == 0):
+                result.append("Fizz")
+            elif((i+1)%5 == 0):
+                result.append("Buzz")
+            else:
+                result.append(str(i+1))
+        return result
